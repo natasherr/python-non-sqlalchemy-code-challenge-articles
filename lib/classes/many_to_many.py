76d73b085eeb79198610eba5cad80d20f603fc89
Ashley Natasha <ashleyotsiula@gmail.com>
2024-12-08 21:05:49 +0300

    py

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,12 +1,11 @@
 class Article:
 
-    all = []
 
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
-        Article.all.append(self)
+       
     
     @property
     def title(self):
